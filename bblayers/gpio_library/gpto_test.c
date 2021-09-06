d9c0c402647f81636d43fdbe80965d1fab695e5f
Joseph Vargas <joseph@josvar.com>
2021-09-06 04:29:07 +0200

    Todo

---
+++ bblayers/gpio_library/gpto_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "gpio.h"
+#define OUTPUT 0
+#define INPUT 1
+
+void intHandler(int dummy) {
+    endGpio();
+    exit(0);
+}
+
+
+int main()
+{
+  signal(SIGINT, intHandler);
+
+  printf("starting gpio\n");
+  startGpio();
+  printf("set 18 output\n");
+  pinMode(18, OUTPUT);
+  printf("set 2 input\n");
+  pinMode(2, INPUT);
+  printf("writting in 18 \n");
+  digitalWrite(18, 1);
+  printf("reading 2\n");
+  digitalRead(2);
+  printf("lets blink madafakas\n");
+  blink(18, 0.5, 30);
+  printf("byeee\n");
+  endGpio();
+}
