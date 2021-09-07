9ff00d84fa7e157b011511ca39a463ac8f1bf9e5
Joseph Vargas <joseph@josvar.com>
2021-09-07 05:07:28 +0200

    Implemented Vite

--- meta-smart-house/recipes-connectivity/house-web-client/files/index.js
+++ meta-smart-house/recipes-connectivity/house-web-client/files/index.js
@@ -1,12 +1,29 @@
 // web server in express
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 80;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const doorsFile = '/home/root/doors_status.txt';
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.static('dist'))
+
+
+// get doors
+app.get('/doors', (_req, res) => {
+    fs.readFile(doorsFile, 'utf8', (err, data) => {
+        if (err) throw err;
+        res.send(data.trim().split(' '));
+
+    });
+});
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
