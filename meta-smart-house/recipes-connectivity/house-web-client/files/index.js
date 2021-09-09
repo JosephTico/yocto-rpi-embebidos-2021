65507df22d904a256ddcf645ba13d31986d98672
Joseph Vargas <joseph@josvar.com>
2021-09-09 12:40:50 -0600

    Finnal ?

--- meta-smart-house/recipes-connectivity/house-web-client/files/index.js
+++ meta-smart-house/recipes-connectivity/house-web-client/files/index.js
@@ -1,5 +1,6 @@
 // web server in express
 const express = require('express');
+const crypto = require("crypto")
 const { exec } = require("child_process");
 const fs = require('fs');
 const app = express();
@@ -8,12 +9,42 @@ const port = 80;
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.static('dist'))
 
+const users = [
+    { "username": "joseph", "password_sha256": "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4" }
+];
+
+function verifyUserPassword(login, password) {
+    let userData = users.find(u => u.username === login.trim());
+    if (userData) {
+        return userData.password_sha256 === crypto.createHash("sha256").update(password).digest("hex");
+    }
+    return false;
+}
+
+// authentication middleware
+app.use((req, res, next) => {
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    // Verify login and password are set and correct
+    if (login && password && verifyUserPassword(login, password)) {
+        // Access granted...
+        return next()
+    }
+
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+    res.status(401).send('Authentication required.') // custom message
+})
+
 
 // get doors
 app.get('/doors', (_req, res) => {
     exec("gpio-client r 4", (err, stdout, stderr) => {
-        if (err) throw err;
-        if (stderr) throw stderr;
+        if (err) console.error(err);
+        if (stderr) console.error(stderr);
         res.send(stdout.trim().replace(/\D/g, '').split('').map((m) => m == "1"));
 
     });
@@ -35,10 +66,18 @@ app.post('/leds', (req, res) => {
 });
 
 
+app.get('/camera', (_req, res) => {
+    exec("fswebcam /opt/house-smart-web/dist/assets/camera.jpg", (err, stdout, stderr) => {
+        if (err) console.error(err);
+        res.redirect('/assets/camera.jpg')
+    });
+});
+
+
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://0.0.0.0:${port}`);
 })
--- meta-smart-house/recipes-connectivity/house-web-client/files/vite_src/src/App.vue
+++ meta-smart-house/recipes-connectivity/house-web-client/files/vite_src/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onBeforeUnmount, watch } from "vue";
 
-let doors = ref({});
+let doors = ref([false, false, false, false]);
 let leds = ref([
   { id: 1, on: false },
   { id: 2, on: false },
@@ -35,6 +35,18 @@ const modifyAllLeds = async (status) => {
   });
 };
 
+
+const insertPhoto = async () => {
+  let img = new Image();
+  let div = document.getElementById('camera-photo');
+
+  img.onload = function() {
+    div.appendChild(img);
+  };
+
+  img.src = '/camera';
+};
+
 function set_interval() {
   polling = setInterval(() => {
     reload();
@@ -54,10 +66,11 @@ onBeforeUnmount(() => {
 <template>
   <div class="graphic">
     <img src="./assets/casita.svg" alt="My casita" />
-    <div class="door" id="d1"></div>
-    <div class="door2" id="d2"></div>
-    <div class="door2" id="d3"></div>
-    <div class="door horizontal" id="d4"></div>
+    <div id="camera" class="glowing" v-on:click="insertPhoto"></div>
+    <div class="door" id="d1" v-bind:class="{ active: doors[0] }"></div>
+    <div class="door2" id="d2" v-bind:class="{ active: doors[1] }"></div>
+    <div class="door2" id="d3" v-bind:class="{ active: doors[2] }"></div>
+    <div class="door horizontal" id="d4" v-bind:class="{ active: doors[3] }"></div>
     <label class="switch" id="s1">
       <input type="checkbox" v-model="leds[0].on" />
       <span class="slider round"></span>
@@ -82,21 +95,11 @@ onBeforeUnmount(() => {
   
 
   <br />
-  <!--ul id="example-1">
-  Doors: {{ doors }}<br />
-  
-    <li v-for="led in leds" :key="led.id">
-      {{ led.id }}: {{ led.on ? "on" : "off" }}
-      
-      <label class="switch">
-        <input type="checkbox" v-model="led.on" />
-        <span class="slider round"></span>
-      </label>
-    </li>
-  </ul>
-  < Rounded switch -->
-  <button v-on:click="modifyAllLeds(false)">Turn off all</button>
+
+  <button v-on:click="modifyAllLeds(false)">Turn off all</button>&nbsp;
   <button v-on:click="modifyAllLeds(true)">Turn on all</button>
+
+  <div id="camera-photo"></div>
 </template>
 
 <style>
@@ -184,13 +187,13 @@ input:checked + .slider:before {
 #s1{
   position: absolute;
   top: 25%;
-  left: 22%;
+  left: 30%;
 }
 
 #s2{
   position: absolute;
   top: 70%;
-  left: 22%;
+  left: 30%;
 }
 
 #s3{
@@ -202,7 +205,7 @@ input:checked + .slider:before {
 #s4{
   position: absolute;
   top: 40%;
-  left: 59%;
+  left: 56.5%;
 }
 
 #s5{
@@ -211,15 +214,15 @@ input:checked + .slider:before {
   left: 57%;
 }
 
-.door:hover {
+.door.active {
     background: green;
     transform: rotate(-90deg);
 }
-.door2:hover {
+.door2.active {
     background: green;
     transform: rotate(90deg);
 }
-.door.horizontal:hover {
+.door.horizontal.active {
     background: green;
     transform: rotate(90deg);
 }
@@ -258,24 +261,62 @@ input:checked + .slider:before {
 }
 
 #d1 {
-  left: 38.3vw;
+  left: 37.9vw;
   top: 11.5vw
 }
 #d2 {
-  left: 38.3vw;
+  left: 37.9vw;
   top: 21.2vw;
  
 }
 #d3 {
-  left: 67.0vw;
+  left: 66.6vw;
   top: 2.5vw
 }
 #d4 {
-  left: 42vw;
+  left: 41.4vw;
   top: 36.1vw
 }
 
 
+@keyframes glow {
+  to {
+    border-color: #69c800;
+    box-shadow: 0 0 5px #69c800;
+  }
+}
+
+.glowing {
+  border: 1px solid transparent;
+  animation: glow 1.0s infinite alternate;  
+}
 
 
+div#camera {
+  background: rgb(0 0 0 / 4%);
+  width: 130px;
+  height: 104px;
+  position: absolute;
+  right: 22%;
+  top: 1.5vw;
+  cursor: pointer;
+}
+
+div#camera:hover {
+  background: rgba(0,0,0,0.3);
+}
+
+#camera-photo {
+  padding: 0 20%;
+  width: 100%;
+  box-sizing: border-box;
+  margin-top: 120px;
+}
+
+#camera-photo img {
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
 </style>
